8b01cf2d61004a1c878a24ec07fdd466c622fbf5
believer <rickard.laurin@gmail.com>
2016-05-24 21:56:20 +0200

    Update webpack.prod.config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,7 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var TransferWebpackPlugin = require('transfer-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const TransferWebpackPlugin = require('transfer-webpack-plugin')
 
 module.exports = {
   devtool: 'source-map',
@@ -26,13 +26,16 @@ module.exports = {
         warnings: false
       }
     }),
+    new webpack.ProvidePlugin({
+      fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch',
+    }),
     new TransferWebpackPlugin([
-      { from: 'images', to: 'images' }
+      { from: 'assets', to: './' }
     ], path.join(__dirname, 'src'))
   ],
   postcss: [
     require('postcss-import')({
-      from: 'src'
+      path: ['src', 'src/styles']
     }),
     require('postcss-css-variables'),
     require('postcss-custom-media'),
@@ -40,7 +43,9 @@ module.exports = {
     require('postcss-for'),
     require('postcss-nested'),
     require('autoprefixer'),
-    require('cssnano')
+    require('cssnano')({
+      zindex: false
+    })
   ],
   module: {
     loaders: [
@@ -51,7 +56,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&sourceMap=true&localIdentName=[name]__[local]___[hash:base64:5]!postcss')
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
@@ -59,11 +71,9 @@ module.exports = {
     modulesDirectories: [
       'src/styles',
       'src/components',
-      'src/stores',
-      'src/actions',
       'node_modules',
       'src'
     ],
-    extensions: ['', '.js', '.css']
+    extensions: ['', '.js', '.css', '.json']
   }
-};
+}
