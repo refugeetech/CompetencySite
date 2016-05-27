eefff0b68b027a3e098f2bd4080eb4285ce0b856
believer <rickard.laurin@gmail.com>
2016-05-27 15:32:40 +0200

    Remove parens

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ ReactDOM.render((
 
     <Route
       component={App}
-      onEnter={(nextState) => {
+      onEnter={nextState => {
         LanguageActions.setLanguage(nextState.params.language)
       }}
       path="/lang/:language" />
