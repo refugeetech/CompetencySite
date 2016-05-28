53083f94f44084f27cf659716a10f5fc0f1f1b10
ambodi <amir.rahnama@gmail.com>
2016-05-28 17:13:36 +0200

    add basic react treemap

--- src/components/Treemap/Treemap.js
+++ src/components/Treemap/Treemap.js
@@ -2,26 +2,49 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import styles from './Treemap.css'
 import Content from '../Content/Content'
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries } from 'react-vis'
+import { Treemap } from 'react-vis'
 
 export const Splash = () =>
-  <div className="container">
+  <div className="container" styleName="container">
     <Content
       flex="2"
       left>
-      <XYPlot
+      <Treemap
+        data={{
+          title: '',
+          children: [
+            {
+              title: 'China',
+              size: 1357
+            },
+            {
+              title: 'India',
+              size: 1252
+            },
+            {
+              title: 'USA',
+              size: 321
+            },
+            {
+              title: 'Indonesia',
+              size: 249.9
+            },
+            {
+              title: 'Brasil',
+              size: 200.4
+            },
+            {
+              title: 'Pakistan',
+              size: 192
+            },
+            {
+              title: 'Nigeria',
+              size: 173.6
+            }
+          ]
+        }}
         height={300}
-        width={300}>
-        <HorizontalGridLines />
-        <LineSeries
-          data={[
-            { x: 1, y: 10 },
-            { x: 2, y: 5 },
-            { x: 3, y: 15 }
-          ]}/>
-        <XAxis />
-        <YAxis />
-      </XYPlot>
+        width={500}/>
     </Content>
   </div>
 
